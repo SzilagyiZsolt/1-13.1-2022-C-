a7cbc0ad6dde8d7a39e67f89cf129cf6f6ccee11
Szilágyi Zsolt <zsoltika20030@gmail.com>
2023-02-06 12:10:44 +0100

    2023-02-06

---
+++ 1-13-1-C/Runparam/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Runparam
+{
+    internal class Program
+    {
+        static void Main(string[] tomb)
+        {
+            if (tomb.Length != 0)
+            {
+                Console.WriteLine("Ezt a paramétert kaptam: {0}", tomb[0]);
+            }
+            else
+            {
+                Console.WriteLine("Nincs megadva paraméter");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1-13-1-C/Számkitaláló játék/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace második_feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Adj meg egy számot: ");
+            a = int.Parse(Console.ReadLine());
+            if (a % 3 == 0) { 
+                Console.WriteLine("A számod amit megadtál osztható hárommal");
+            }
+            if (a % 4 == 0)
+            {
+                Console.WriteLine("A számod amit megadtál osztható néggyel");
+            }
+            if (a % 9 == 0)
+            {
+                Console.WriteLine("A számod amit megadtál osztható kilenccel");
+            }
+            Console.ReadKey();
+        }
+    }
+}
