d6f918ad102b1691edf20ed6f6664e297e1438c4
Szilágyi <zsoltika20030@gmail.com>
2023-02-07 16:54:37 +0100

    2023-02-07

---
+++ 9. Feladat/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.Feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, c;
+            Console.WriteLine("Add meg a CO2-t: ");
+            a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Add meg O2-t: ");
+            b = float.Parse(Console.ReadLine());
+            c = (a / b);
+            if (0.8 < c)
+            {
+                Console.WriteLine("Szénhidrátokból nyeri");
+            }
+            else if (0.8 > c)
+            {
+                Console.WriteLine("Zsírból nyeri");
+            }
+            else
+            {
+                Console.WriteLine("Tökéletes");
+            }
+            Console.ReadKey();
+        }
+    }
+}
