1594847d30cb7294325336f4edcd9d3913177db3
Szilágyi Zsolt <zsoltika20030@gmail.com>
2023-02-10 11:06:48 +0100

    2023-02-10

---
+++ 1-13-1-C/Teszt/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teszt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            bool False=true;
+            try
+            {
+                Console.WriteLine("Adj egy számot: ");
+                a=int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Ez nem szám!");
+                False=false;
+            }
+            
+            if (False)
+            {
+                Console.WriteLine("Igaz");
+            }
+            else
+            {
+                Console.WriteLine("Nem volt igaz");
+                Console.WriteLine("És még rossz is");
+            }
+            Console.ReadLine();
+        }
+    }
+}
