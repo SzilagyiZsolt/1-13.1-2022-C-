e2330cae850a02750ac739c5330dc539f9b32692
Szilágyi <zsoltika20030@gmail.com>
2023-03-19 19:56:35 +0100

    2023-03-19

---
+++ 1-13-1-C/PontHatOOP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PontHatOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Add meg a pontodat: ");
+            a = int.Parse(Console.ReadLine());
+            PontHatOOP P1 = new PontHatOOP();
+            P1.setPont(a);
+            P1.setJegy();
+
+            Console.ReadKey();
+        }
+    }
+}
