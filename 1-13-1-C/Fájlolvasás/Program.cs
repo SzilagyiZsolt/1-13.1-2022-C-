5912b8430d86be541b0bc300b07550700dabc512
Szilágyi Zsolt <zsoltika20030@gmail.com>
2023-04-17 13:22:36 +0200

    2023-04-17

---
+++ 1-13-1-C/Fájlolvasás/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fájlolvasás
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Név;
+            Console.WriteLine("Fájlolvasás!");
+            Név=Console.ReadLine();
+            StreamReader Munkafüzet1 = File.OpenText(@Név);
+            while (Munkafüzet1.Peek() != -1)
+            {
+                Console.WriteLine(Munkafüzet1.ReadLine());
+            }
+            Console.ReadKey();
+        }
+    }
+}
